83e5af0972a3adadbbd1dab4c917d5e9e84376c1
Martin-L-F <Martin-L-F@users.noreply.github.com>
2021-05-05 16:07:06 +0200

    Made files for Control Unit

---
+++ ControlUnit/ControlUnitApp/ControlUnit_Controller.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <functional>
+#include <thread>
+
+class ControlUnit_Controller
+{
+public:
+    ControlUnit_Controller()
+    {
+    }
+
+private:
+};
---
+++ ControlUnit/ControlUnitApp/main.cpp
@@ -0,0 +1,8 @@
+#include "ControlUnit_Controller.hpp"
+
+int main(){
+    std::cout << "[INFO] Initiating system" << std::endl;
+    ControlUnit_Controller ControlUnit;
+    std::cin.get();
+    return 0;
+}
