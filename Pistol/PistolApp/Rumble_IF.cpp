913939e4eef56e72194d4d1d45a78f7a18122477
Oskari Tuormaa <oskaritu@gmail.com>
2021-06-03 02:48:10 +0200

    Updated Rumble_IF to use new kernel functionality

--- Pistol/PistolApp/Rumble_IF.cpp
+++ Pistol/PistolApp/Rumble_IF.cpp
@@ -1,17 +1,17 @@
 #include "Rumble_IF.hpp"
 
-void Rumble_IF::startRumble()
-{
-	try
-	{
-		FILE *fp;
+void Rumble_IF::startRumble() {
+  try {
+    FILE *fp;
+    char msg[32];
 
-		fp = fopen("/dev/RUMBLE", "w");
-		fputc('1', fp);
-		fclose(fp);
-	}
-	catch (const std::exception &ex)
-	{
-		std::cout << "[Rumble_IF][ERROR] Could not write to file. Details: " << ex.what() << "." << std::endl;
-	}
+    sprintf(msg, "%d", _rumble_time);
+
+    fp = fopen("/dev/RUMBLE", "w");
+    fputs(msg, fp);
+    fclose(fp);
+  } catch (const std::exception &ex) {
+    std::cout << "[Rumble_IF][ERROR] Could not write to file. Details: "
+              << ex.what() << "." << std::endl;
+  }
 }
